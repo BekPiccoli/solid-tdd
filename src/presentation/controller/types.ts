e46cc536d856fd596c9c4ef09dd4778c343e4a75
Bernardo Piccoli <bernardo@redrive.com.br>
2025-12-05 18:17:16 -0300

    feat: add types for handleSignup

---
+++ src/presentation/controller/types.ts
@@ -0,0 +1,11 @@
+export interface HttpRequest {
+  body: {
+    name: string
+    email: string
+    password: string
+    passwordConfirmation: string
+  }
+}
+export interface HttpResponse {
+  statusCode: number
+}
